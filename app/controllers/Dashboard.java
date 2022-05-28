d0148df238d0f1a8a0b59e114e9603ef2661f378
Ian Blake <ian@ianbl8.com>
2022-05-28 19:37:44 +0100

    Latitude and longitude added to Station

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -15,8 +15,8 @@ public class Dashboard extends Controller {
     render("dashboard.html", stations);
   }
 
-  public static void addStation(String name) {
-    Station station = new Station (name);
+  public static void addStation(String name, double latitude, double longitude) {
+    Station station = new Station (name, latitude, longitude);
     Logger.info("Adding Station: " + name);
     station.save();
     redirect("/dashboard");
